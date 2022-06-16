f2c933bb78e6d52e10a533cd4a8736f06818be16
yc666 <1347044537@qq.com>
2022-06-16 20:58:29 +0800

    feat：设计主界面的布局，整体框架。
    建立Home组件，修改配置文件。

--- Front-WorldOfZuul/world_of_zuul/src/App.vue
+++ Front-WorldOfZuul/world_of_zuul/src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Home/>
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Home
   }
 }
 </script>
