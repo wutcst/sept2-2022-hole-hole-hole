1ab76ebfe2c9229ec3900c6b183b973284a7621b
PpxiA6 <820769446@qq.com>
2022-06-27 12:29:03 +0800

    feat(entity): 创建了用户类
    创建用户类
    
    Closes #23

---
+++ Back-WorldOfZuul/WorldOfZuul/world-of-zuul/src/main/java/cn/lkpttxg/sept2/worldofzuul/settings/entity/user/User.java
@@ -0,0 +1,36 @@
+package cn.lkpttxg.sept2.worldofzuul.settings.entity.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 用户实体类
+ *
+ * @author PpxiA
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+  /**
+   * 用户id
+   */
+  private String userId;
+  /**
+   * 用户绑定玩家id
+   */
+  private String playerId;
+  /**
+   * 用户密码
+   */
+  private String password;
+  /**
+   * 用户名
+   */
+  private String username;
+  /**
+   * 用户邮箱
+   */
+  private String email;
+}
