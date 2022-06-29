3a5dbdaa78f877aa4f19b15042aeab99a1a1f119
PpxiA6 <820769446@qq.com>
2022-06-29 14:11:17 +0800

    feat(controller): 准备合并
    准备合并。
    
    Closes #30

--- Back-WorldOfZuul/WorldOfZuul/world-of-zuul/src/main/java/cn/lkpttxg/sept2/worldofzuul/workbench/web/controller/player/PlayerController.java
+++ Back-WorldOfZuul/WorldOfZuul/world-of-zuul/src/main/java/cn/lkpttxg/sept2/worldofzuul/workbench/web/controller/player/PlayerController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 玩家controller
+ * 玩家controller.
  *
  * @author TXG & PpxiA
  */
@@ -29,11 +29,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/player")
 public class PlayerController {
+
   @Resource
   private PlayerService playerService;
   @Resource
   private Game game;
 
+  /**
+   * 玩家拾取物品controller.
+   *
+   * @param playerId 玩家id
+   * @param location 物品位置
+   * @return 请求状态码和玩家对象
+   */
   @ApiOperation(value = "拾取物品", notes = "拾取指定位置的物品")
   @ApiImplicitParams({
       @ApiImplicitParam(name = "playerId", value = "玩家编号",
@@ -41,80 +49,102 @@ public class PlayerController {
       @ApiImplicitParam(name = "location", value = "物品坐标",
           required = true, paramType = "query", dataType = "String")})
   @ApiResponses({
-      @ApiResponse(code = 200,message = "成功拾取"),
-      @ApiResponse(code = 400,message = "此位置没有物品"),
-      @ApiResponse(code = 404,message = "拾取失败，超重")
+      @ApiResponse(code = 200, message = "成功拾取"),
+      @ApiResponse(code = 400, message = "此位置没有物品"),
+      @ApiResponse(code = 404, message = "拾取失败，超重")
   })
   @GetMapping("pick/{playerId}")
-  public ResponseData<Player> pick(@PathVariable(value = "playerId") String playerId, Integer location){
+  public ResponseData<Player> pick(@PathVariable(value = "playerId") String playerId,
+      Integer location) {
     Player player = game.getPlayer(playerId);
     Integer result = playerService.pick(player, location);
-    if(result.equals(ActionResult.MISS)){
+    if (result.equals(ActionResult.MISS)) {
       return ResultGenerator.genOtherResult(ResultCode.FAIL, "此位置没有物品！");
-    }else if(result.equals(ActionResult.OVERWEIGHT)){
+    } else if (result.equals(ActionResult.OVERWEIGHT)) {
       return ResultGenerator.genFailResult("拾取失败，超重！");
-    }else{
+    } else {
       return ResultGenerator.genSuccessResult(player);
     }
   }
 
+  /**
+   * 玩家丢弃物品controller.
+   *
+   * @param playerId 玩家id
+   * @param id 物品编号
+   * @return 请求状态码和玩家对象
+   */
   @ApiOperation(value = "丢弃物品", notes = "丢弃指定的物品")
   @ApiImplicitParams({
       @ApiImplicitParam(name = "playerId", value = "玩家编号",
           required = true, paramType = "path", dataType = "String"),
       @ApiImplicitParam(name = "id", value = "物品编号",
-          required = true, paramType = "query", dataType = "String"),})
+          required = true, paramType = "query", dataType = "String")})
   @ApiResponses({
-      @ApiResponse(code = 200,message = "成功丢弃"),
+      @ApiResponse(code = 200, message = "成功丢弃"),
   })
   @GetMapping("throw/{playerId}")
   public ResponseData<Player> throwAway(@PathVariable(value = "playerId") String playerId,
-      String id){
+      String id) {
     Player player = game.getPlayer(playerId);
     playerService.throwAway(player, id);
     return ResultGenerator.genSuccessResult(player);
   }
 
+  /**
+   * 玩家吃物品controller.
+   *
+   * @param playerId 玩家id
+   * @param id 物品编号
+   * @return 请求状态码和玩家对象
+   */
   @ApiOperation(value = "吃食物", notes = "吃指定的物品")
   @ApiImplicitParams({
       @ApiImplicitParam(name = "playerId", value = "玩家编号",
           required = true, paramType = "path", dataType = "String"),
       @ApiImplicitParam(name = "id", value = "物品编号",
-          required = true, paramType = "query", dataType = "String"),})
+          required = true, paramType = "query", dataType = "String")})
   @ApiResponses({
-      @ApiResponse(code = 200,message = "成功吃"),
-      @ApiResponse(code = 404,message = "物品不能吃"),
+      @ApiResponse(code = 200, message = "成功吃"),
+      @ApiResponse(code = 404, message = "物品不能吃"),
   })
   @GetMapping("eat/{playerId}")
   public ResponseData<Player> eat(@PathVariable(value = "playerId") String playerId,
-      String id){
+      String id) {
     Player player = game.getPlayer(playerId);
     Integer result = playerService.eat(player, id);
-    if(result.equals(ActionResult.UNMATCH)){
+    if (result.equals(ActionResult.UNMATCH)) {
       return ResultGenerator.genFailResult("物品不能吃！");
-    }else{
+    } else {
       return ResultGenerator.genSuccessResult(player);
     }
   }
 
+  /**
+   * 玩家装备物品controller.
+   *
+   * @param playerId 玩家id
+   * @param id 物品编号
+   * @return 请求状态码和玩家对象
+   */
   @ApiOperation(value = "装备武器", notes = "装备指定的物品")
   @ApiImplicitParams({
       @ApiImplicitParam(name = "playerId", value = "玩家编号",
           required = true, paramType = "path", dataType = "String"),
       @ApiImplicitParam(name = "id", value = "物品编号",
-          required = true, paramType = "query", dataType = "String"),})
+          required = true, paramType = "query", dataType = "String")})
   @ApiResponses({
-      @ApiResponse(code = 200,message = "成功装备"),
-      @ApiResponse(code = 404,message = "物品不能装备"),
+      @ApiResponse(code = 200, message = "成功装备"),
+      @ApiResponse(code = 404, message = "物品不能装备"),
   })
   @GetMapping("equip/{playerId}")
   public ResponseData<Player> equip(@PathVariable(value = "playerId") String playerId,
-    String id){
+      String id) {
     Player player = game.getPlayer(playerId);
     Integer result = playerService.equipWeapon(player, id);
-    if(result.equals(ActionResult.UNMATCH)){
+    if (result.equals(ActionResult.UNMATCH)) {
       return ResultGenerator.genFailResult("此物品无法装备！");
-    }else{
+    } else {
       return ResultGenerator.genSuccessResult(player);
     }
   }
